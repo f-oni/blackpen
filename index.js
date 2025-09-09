d56903f38b6b063abca413141ace132eeb70c168
folusho <folushotemi8@gmail.com>
2025-09-09 21:09:49 +0100

    index file updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {  
-     res.send('Hello World! we made it');});
+     res.send('Hello World! We made it. Devops is interesting');});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
